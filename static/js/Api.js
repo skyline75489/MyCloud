7e15b37dc2ce884d6ddfd2eef9cb6d399b07519f
skyline75489 <skyline75489@outlook.com>
2015-06-10 20:27:50 +0800

    update

---
+++ static/js/Api.js
@@ -0,0 +1,49 @@
+var Api = {
+  baseURL: 'http://localhost:5000',
+  doPostRequest: function(url, payload, callback) {
+     $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'POST',
+      data: JSON.stringify(payload),
+      contentType: "application/json; charset=utf-8",
+      success: function(data) {
+        console.log(url, status, data);
+        if (data.message === 'OK') {
+          callback && callback(data)
+        }
+      },
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }
+    });
+  },
+  doGetRequest: function(url, callback) {
+     $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        console.log(url, status, data);
+        if (data.message === 'OK') {
+          callback && callback(data)
+        }
+      },
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }
+    });
+  },
+  doLogin: function(payload, callback) {
+    this.doPostRequest(this.baseURL + '/login', payload, callback);
+  },
+  testAuth: function(callback) {
+    this.doGetRequest(this.baseURL + '/login/testAuth', callback);
+  },
+  getFolders: function(callback) {
+    this.doGetRequest(this.baseURL + '/folders', callback)
+  },
+  addFolder: function(payload, callback) {
+    this.doPostRequest(this.baseURL + '/folders', payload, callback);
+  }
+}
