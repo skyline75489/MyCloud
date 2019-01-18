2df41dc71c545910d8a5979654216dffc6225438
Chester Liu <skyline75489@outlook.com>
2019-01-18 11:52:23 +0800

    Hello React Hooks

--- cloud-disk-app/src/Component/LoginForm.js
+++ cloud-disk-app/src/Component/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Col from 'react-bootstrap/lib/Col';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
@@ -8,63 +8,52 @@ import './LoginForm.css';
 
 import Api from '../Logic/Api';
 
-class LoginForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: false
-        }
+const LoginForm = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState(false);
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        const email = this.email
-        const password = this.password;
-
+    const login = () => {
         Api.login(email, password)
         .then(response => {
             if (!response.ok) {
-                this.setState({ error: true });
+                setLoginError(true);
                 return;
             }
 
             response.json()
-            .then(data => this.props.onLogin(data))
+            .then(data => props.onLogin(data))
         });
     }
 
-    render() {
-        var alert;
-        if (this.state.error) {
-            alert = (
-              <Alert bsStyle='danger'>
-                <strong>Error: </strong>Wrong email or password.
-              </Alert>
-            );
-          } else {
-            alert = <span></span>;
-        }
-
-        return (
-            <Col md={4} mdOffset={4}>
-                <h3>Login</h3>
-                {alert}
-                <form id="loginForm" onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                        <FormControl type="text" placeholder="Email" onChange={evt => this.email = evt.target.value} />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl type="password" placeholder="Password" onChange={evt => this.password = evt.target.value} />
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl id="submitButton" type="submit" value="Login" /> 
-                    </FormGroup>
-                </form>
-            </Col>
-      );
+    var alert;
+    if (loginError) {
+        alert = (
+            <Alert bsStyle='danger'>
+            <strong>Error: </strong>Wrong email or password.
+            </Alert>
+        );
+        } else {
+        alert = <span></span>;
     }
+
+    return (
+        <Col md={4} mdOffset={4}>
+            <h3>Login</h3>
+            {alert}
+            <form id="loginForm" onSubmit={(e) => { e.preventDefault(); login(); }}>
+                <FormGroup>
+                    <FormControl type="text" placeholder="Email" onChange={(evt) => setEmail(evt.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                    <FormControl type="password" placeholder="Password" onChange={(evt) => setPassword(evt.target.value)} />
+                </FormGroup>
+                <FormGroup>
+                    <FormControl id="submitButton" type="submit" value="Login" /> 
+                </FormGroup>
+            </form>
+        </Col>
+    );
 }
 
 export default LoginForm;
